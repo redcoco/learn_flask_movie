379b21f8e66f9af1b91b91743bf56ddc5b9fa15c
redcoco <1849737243@qq.com>
2017-09-25 16:22:25 +0800

    管理员-日志管理的列表查看,删除功能

--- app/admin/views.py
+++ app/admin/views.py
@@ -2,7 +2,7 @@
 from . import admin
 from flask import render_template, redirect, url_for, flash, session, request
 from app.admin.forms import LoginForm, TagForm, MovieForm, PreviewForm
-from app.models import Admin, Tag, Movie, Preview, User, Comment,Moviecol
+from app.models import Admin, Tag, Movie, Preview, User, Comment, Moviecol, Oplog, Adminlog, Userlog
 from functools import wraps
 from app.exts import db
 from manage import app
@@ -21,6 +21,15 @@ def admin_login_req(f):
         return f(*args, **kwargs)
 
     return decorated_function
+
+
+# 上下文应用处理器
+@admin.context_processor
+def tpl_extra():
+    data = dict(
+        online_time=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    )
+    return data
 
 
 # 管理员主页
@@ -42,6 +51,13 @@ def login():
             flash("密码错误！", "err")
             return redirect(url_for("admin.login"))
         session["admin"] = data["account"]  # 写入session信息，作为检验登录是否的标签
+        session["admin_id"] = admin.id
+        adminlog = Adminlog(
+            admin_id=admin.id,
+            ip=request.remote_addr
+        )
+        db.session.add(adminlog)  # 保存管理员登录日志
+        db.session.commit()
         return redirect(request.args.get("next") or url_for("admin.index"))
     return render_template("admin/login.html", form=form)
 
@@ -51,6 +67,7 @@ def login():
 @admin_login_req
 def logout():
     session.pop("admin", None)
+    session.pop("admin_id", None)  # 保存管理员登录日志
     return redirect(url_for("admin.login"))
 
 
@@ -78,6 +95,13 @@ def tag_add():
         db.session.add(tag)
         db.session.commit()
         flash("添加标签成功", "ok")
+        oplog = Oplog(
+            admin_id = session["admin_id"],
+            ip = request.remote_addr,
+            reason = "添加标签%s" % data["name"],
+        )
+        db.session.add(oplog)  # 操作日志
+        db.session.commit()
         redirect(url_for("admin.tag_add"))
     return render_template("admin/tag_add.html", form=form)
 
@@ -395,24 +419,43 @@ def moviecol_del(id=None):
 
 
 # 操作日志列表
-@admin.route("/oplog/list/")
+@admin.route("/oplog/list/<int:page>/", methods=["GET"])
 @admin_login_req
-def oplog_list():
-    return render_template("admin/oplog_list.html")
+def oplog_list(page=None):
+    if page is None:
+        page=1
+    page_data = Oplog.query.join(Admin).filter(
+        Admin.id == Oplog.admin_id).order_by(
+        Oplog.addtime.desc()
+    ).paginate(page=page, per_page=10)
+    return render_template("admin/oplog_list.html",page_data=page_data)
 
 
 # 管理员日志列表
-@admin.route("/adminloginlog/list/")
+@admin.route("/adminloginlog/list/<int:page>/", methods=["GET"])
 @admin_login_req
-def adminloginlog_list():
-    return render_template("admin/adminloginlog_list.html")
+def adminloginlog_list(page=None):
+    if page is None:
+        page = 1
+    page_data = Adminlog.query.join(Admin).filter(
+        Admin.id == Adminlog.admin_id).order_by(
+        Adminlog.addtime.desc()
+    ).paginate(page=page, per_page=10)
+    return render_template("admin/adminloginlog_list.html",page_data=page_data)
 
 
 # 会员日志列表
-@admin.route("/userloginlog/list/")
+@admin.route("/userloginlog/list/<int:page>/", methods=["GET"])
 @admin_login_req
-def userloginlog_list():
-    return render_template("admin/userloginlog_list.html")
+def userloginlog_list(page=None):
+    if page is None:
+        page = 1
+    page_data = Userlog.query.join(
+        User).filter(
+        User.id == Userlog.user_id).order_by(
+        Userlog.addtime.desc()
+    ).paginate(page=page, per_page=10)
+    return render_template("admin/userloginlog_list.html",page_data=page_data)
 
 
 # 添加角色
