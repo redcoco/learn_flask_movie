daafc0ab5e8cc54e9f70fcde6965ed5af4712017
redcoco <1849737243@qq.com>
2017-10-02 11:46:13 +0800

    主页标签及电影列表筛选

--- app/home/views.py
+++ app/home/views.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 from . import home
-from flask import render_template, redirect, url_for, flash,session,request
-from .forms import RegisterForm,LoginForm,UserdetailForm,PwdForm
-from app.models import User,Userlog,Preview
+from flask import render_template, redirect, url_for, flash, session, request
+from .forms import RegisterForm, LoginForm, UserdetailForm, PwdForm
+from app.models import User, Userlog, Preview, Tag, Movie
 from werkzeug.security import generate_password_hash
 from app.exts import db
 from functools import wraps
@@ -15,10 +15,11 @@ from manage import app
 
 def user_login_req(f):
     @wraps(f)
-    def decorated_function(*args,**kwargs):
+    def decorated_function(*args, **kwargs):
         if 'user' not in session:
-            return redirect(url_for('home.login',next=request.url))
-        return f(*args,**kwargs)
+            return redirect(url_for('home.login', next=request.url))
+        return f(*args, **kwargs)
+
     return decorated_function
 
 
@@ -33,18 +34,18 @@ def login():
             flash('请检查用户名!', 'err')
             return redirect(url_for('home.login'))
         if not user.check_pwd(data['pwd']):
-            flash('密码错误!','err')
+            flash('密码错误!', 'err')
             return redirect(url_for('home.login'))
         session['user'] = user.name
         session['user_id'] = user.id
         userlog = Userlog(
-            user_id = user.id,
+            user_id=user.id,
             ip=request.remote_addr
         )
         db.session.add(userlog)
         db.session.commit()
         return redirect(url_for('home.user'))
-    return render_template("home/login.html",form=form)
+    return render_template("home/login.html", form=form)
 
 
 # 会员退出登录
@@ -79,7 +80,7 @@ def regist():
 def user():
     form = UserdetailForm()
     user = User.query.get_or_404(int(session['user_id']))
-    form.face.validators=[]
+    form.face.validators = []
     if request.method == 'GET':
         form.name.data = user.name
         form.email.data = user.email
@@ -90,13 +91,13 @@ def user():
         file_face = secure_filename(form.face.data.filename)
         if not os.path.exists(app.config['FC_DIR']):
             os.makedirs(app.config['FC_DIR'])
-            os.chmod(app.config['FC_DIR'],'rw')
+            os.chmod(app.config['FC_DIR'], 'rw')
         user.face = change_filename(file_face)
-        form.face.data.save(app.config['FC_DIR']+user.face)
+        form.face.data.save(app.config['FC_DIR'] + user.face)
 
         name_count = User.query.filter_by(name=data['name']).count()
-        if data['name'] != user.name and name_count==1:
-            flash('昵称已经存在！','err')
+        if data['name'] != user.name and name_count == 1:
+            flash('昵称已经存在！', 'err')
             return redirect(url_for('home.user'))
 
         email_count = User.query.filter_by(email=data['email']).count()
@@ -115,9 +116,9 @@ def user():
         user.info = data['info']
         db.session.add(user)
         db.session.commit()
-        flash('修改成功!','ok')
+        flash('修改成功!', 'ok')
         return redirect(url_for('home.user'))
-    return render_template("home/user.html",form=form,user=user)
+    return render_template("home/user.html", form=form, user=user)
 
 
 # 修改密码
@@ -130,14 +131,14 @@ def pwd():
         user = User.query.filter_by(name=session['user']).first()
         from werkzeug.security import generate_password_hash
         if not user.check_pwd(data['old_pwd']):
-            flash('旧密码错误!','err')
+            flash('旧密码错误!', 'err')
             return redirect(url_for('home.pwd'))
-        user.pwd=generate_password_hash(data['new_pwd'])
+        user.pwd = generate_password_hash(data['new_pwd'])
         db.session.add(user)
         db.session.commit()
-        flash('修改密码成功，请重新登录！','ok')
+        flash('修改密码成功，请重新登录！', 'ok')
         return redirect(url_for('home.logout'))
-    return render_template("home/pwd.html",form=form)
+    return render_template("home/pwd.html", form=form)
 
 
 # 评论
@@ -152,11 +153,11 @@ def comments():
 @user_login_req
 def loginlog(page=None):
     if page is None:
-        page=1
-    page_data=Userlog.query.filter_by(user_id=int(session['user_id'])).order_by(
+        page = 1
+    page_data = Userlog.query.filter_by(user_id=int(session['user_id'])).order_by(
         Userlog.addtime.desc()
-    ).paginate(page=page,per_page=10)
-    return render_template("home/loginlog.html",page_data=page_data)
+    ).paginate(page=page, per_page=10)
+    return render_template("home/loginlog.html", page_data=page_data)
 
 
 # 收藏电影
@@ -167,17 +168,79 @@ def moviecol():
 
 
 # 首页
-@home.route('/')
-def index():
-    return render_template("home/index.html")
+@home.route('/', methods=["GET"])
+@home.route('/<int:page>/', methods=["GET"])
+def index(page=None):
+    tags = Tag.query.all()
+    page_data = Movie.query
+
+    # 标签
+    tid = request.args.get("tid", 0)
+    if int(tid) != 0:
+        page_data = page_data.filter_by(tag_id=int(tid))
+
+    # 星级
+    star = request.args.get("star", 0)
+    if int(star) != 0:
+        page_data = page_data.filter_by(star=int(star))
+
+    # 时间
+    time = request.args.get("time", 0)
+    if int(time) != 0:
+        if int(time) == 1:
+            page_data = page_data.order_by(
+                Movie.addtime.desc()
+            )
+        else:
+            page_data = page_data.order_by(
+                Movie.addtime.asc()
+            )
+
+    # 播放量
+    pm = request.args.get("pm", 0)
+    if int(pm) != 0:
+        if int(pm) == 1:
+            page_data = page_data.order_by(
+                Movie.playnum.desc()
+            )
+        else:
+            page_data = page_data.order_by(
+                Movie.playnum.asc()
+            )
+
+    # 评论量
+    cm = request.args.get("cm", 0)
+    if int(cm) != 0:
+        if int(cm) == 1:
+            page_data = page_data.order_by(
+                Movie.commentnum.desc()
+            )
+        else:
+            page_data = page_data.order_by(
+                Movie.commentnum.asc()
+            )
+
+    if page is None:
+        page = 1
+    page_data = page_data.paginate(page=page, per_page=8)
+
+    p = dict(
+        tid=tid,
+        star=star,
+        time=time,
+        pm=pm,
+        cm=cm
+    )
+
+    return render_template("home/index.html", p=p, tags=tags, page_data=page_data)
 
 
 # 电影列表
 # 上映预告
 @home.route('/animation/')
 def animation():
-    data=Preview.query.all()
-    return render_template("home/animation.html",data=data)
+    data = Preview.query.all()
+    return render_template("home/animation.html", data=data)
 
 
 # 电影搜索
